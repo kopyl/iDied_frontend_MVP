c2d1006225a9870dcb5a9662ae13a11e88f1999e
kopyl <oleggggg1997@gmail.com>
2022-03-12 01:33:49 +0200

    Remove redundant import

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core"
-import { HttpClient, HttpParams, HttpErrorResponse } from "@angular/common/http"
+import { HttpClient } from "@angular/common/http"
 import { Title } from "@angular/platform-browser"
 import { Router } from "@angular/router"
 import { GoogleAuthService } from "../auth/google/google-auth.service"
