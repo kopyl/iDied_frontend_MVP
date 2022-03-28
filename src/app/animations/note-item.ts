0c790003b71e236436234fbe933caa50f1779ea3
kopyl <oleggggg1997@gmail.com>
2022-03-28 19:46:22 +0300

    Code formatting

--- src/app/animations/note-item.ts
+++ src/app/animations/note-item.ts
@@ -1,14 +1,24 @@
-import { animate, state, style, transition, trigger } from "@angular/animations"
+import {
+    animate,
+    query,
+    stagger,
+    state,
+    style,
+    transition,
+    trigger,
+} from "@angular/animations"
 
 export const noteItem = [
-    trigger('note-item', [
+    trigger("note-item", [
+        state(
+            "void",
+            style({ position: "relative", top: "-50px", marginBottom: "-55px" })
+        ),
+        state(
+            "*",
+            style({ position: "relative", top: "0px", marginBottom: "5px" })
+        ),
 
-        state('void', style({position: "relative", top: "-50px", marginBottom: "-55px"})),
-        state('*', style({position: "relative", top: "0px", marginBottom: "5px"})),
-
-        transition('* <=> void', [
-            animate(100),
-        ])
-
-    ])
+        transition("* <=> void", [animate(100)]),
+    ]),
 ]
