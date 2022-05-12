ab18a554952191339f2c60ce22507a7db1553296
kopyl <oleggggg1997@gmail.com>
2022-05-12 18:40:11 +0300

    Connect Google Analytics and Clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,48 @@
 import { Component, OnInit } from "@angular/core"
 import { OnlineUpdaterService } from "@services/online-updater"
-import { RouterOutlet } from "@angular/router"
+import { NavigationEnd, Router, RouterOutlet } from "@angular/router"
 import { slider } from "@animations"
+import { environment } from "@environment"
+
+declare const gtag: Function // Google Analytics
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.sass"],
-    animations: slider
+    animations: slider,
 })
 export class AppComponent implements OnInit {
     constructor(
         private onlineUpdater: OnlineUpdaterService,
-    ) {}
+        public router: Router
+    ) {
+        this.setUpGoogleAnalytics()
+    }
+
+    setUpGoogleAnalytics(): void {
+
+        if (!environment.production) return
+
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                gtag("config", "MEASUREMENT-ID", {
+                    page_path: event.urlAfterRedirects,
+                })
+            }
+        })
+    }
 
     ngOnInit(): void {
         this.onlineUpdater.schedule()
     }
 
-    prepareOutlet(outlet: RouterOutlet) {  // id.doc.id#3
-        return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation']
+    prepareOutlet(outlet: RouterOutlet) {
+        // id.doc.id#3
+        return (
+            outlet &&
+            outlet.activatedRouteData &&
+            outlet.activatedRouteData["animation"]
+        )
     }
 }
