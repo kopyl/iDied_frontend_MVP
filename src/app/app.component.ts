1ee1d6b64a6dc0afcf7ba8d407dcd92a9fa7c20f
kopyl <oleggggg1997@gmail.com>
2022-09-18 02:28:03 +0300

    Make getting a language from params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,60 @@
-import { Component, OnInit } from "@angular/core"
-import { OnlineUpdaterService } from "@services/online-updater"
-import { NavigationEnd, Router, RouterOutlet } from "@angular/router"
-import { slider } from "@animations"
-import { environment } from "@environment"
+import { Component, OnInit } from '@angular/core'
+import { OnlineUpdaterService } from '@services/online-updater'
+import {
+    NavigationEnd,
+    Router,
+    RouterOutlet,
+    ActivatedRoute,
+} from '@angular/router'
+import { slider } from '@animations'
+import { environment } from '@environment'
 
 declare const gtag: Function // Google Analytics
 
 @Component({
-    selector: "app-root",
-    templateUrl: "./app.component.html",
-    styleUrls: ["./app.component.sass"],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass'],
     animations: slider,
 })
 export class AppComponent implements OnInit {
     constructor(
         private onlineUpdater: OnlineUpdaterService,
-        public router: Router
+        public router: Router,
+        private route: ActivatedRoute
     ) {
         this.setUpGoogleAnalytics()
     }
 
     setUpGoogleAnalytics(): void {
-
         if (!environment.production) return
 
         this.router.events.subscribe((event) => {
             if (event instanceof NavigationEnd) {
-                gtag("config", "G-X9BGV3FKZ8", {
+                gtag('config', 'G-X9BGV3FKZ8', {
                     page_path: event.urlAfterRedirects,
                 })
             }
         })
     }
 
+    setLanguage() {
+        this.route.queryParams.subscribe((params) => {
+            const lang = params['lang']
+            if (lang === 'en' || lang === 'ua') {
+                localStorage.setItem('lang', lang)
+            }
+        })
+    }
+
+    getLanguage() {
+        return localStorage.getItem('lang') || 'en'
+    }
+
     ngOnInit(): void {
         this.onlineUpdater.schedule()
+        this.setLanguage()
+        this.getLanguage()
     }
 
     prepareOutlet(outlet: RouterOutlet) {
@@ -42,7 +62,7 @@ export class AppComponent implements OnInit {
         return (
             outlet &&
             outlet.activatedRouteData &&
-            outlet.activatedRouteData["animation"]
+            outlet.activatedRouteData['animation']
         )
     }
 }
