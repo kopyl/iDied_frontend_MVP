27996157eec5faedb257bf4b628721eee4331b20
kopyl <oleggggg1997@gmail.com>
2022-09-17 21:52:20 +0300

    Make logging for test

--- src/app/components/sharing/sharing.component.ts
+++ src/app/components/sharing/sharing.component.ts
@@ -1,23 +1,23 @@
-import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core"
-import { MatSnackBar } from "@angular/material/snack-bar"
-import { ConfirmPopupComponent } from "@components/confirmation-popup"
-import { RequestsService } from "@services/requests"
-import { environment } from "@environment"
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ConfirmPopupComponent } from '@components/confirmation-popup';
+import { RequestsService } from '@services/requests';
+import { environment } from '@environment';
 
 @Component({
-    selector: "sharing",
-    templateUrl: "./sharing.component.html",
-    styleUrls: ["./sharing.component.sass"],
+    selector: 'sharing',
+    templateUrl: './sharing.component.html',
+    styleUrls: ['./sharing.component.sass'],
 })
 export class SharingComponent implements OnInit {
-    baseUrl = environment.baseUrl
+    baseUrl = environment.baseUrl;
 
-    loaderVisible = false
+    loaderVisible = false;
 
-    @Input("activeNote") activeNote: frontendNote
-    @Input("confirmPopup") confirmPopup: ConfirmPopupComponent
+    @Input('activeNote') activeNote: frontendNote;
+    @Input('confirmPopup') confirmPopup: ConfirmPopupComponent;
 
-    @Output() sharingCloseEvent = new EventEmitter()
+    @Output() sharingCloseEvent = new EventEmitter();
 
     constructor(
         private readonly requests: RequestsService,
@@ -25,17 +25,17 @@ export class SharingComponent implements OnInit {
     ) {}
 
     get sharingLink(): string {
-        return `${this.baseUrl}/n/${this.activeNote.sharingToken}`
+        return `${this.baseUrl}/n/${this.activeNote.sharingToken}`;
     }
 
     closeSharingWindow(): void {
-        this.sharingCloseEvent.emit()
+        this.sharingCloseEvent.emit();
     }
 
     ngOnInit(): void {}
 
     copyLink(): void {
-        const link = this.sharingLink
+        const link = this.sharingLink;
         // this.clipboard.c
         // await navigator.clipboard.writeText(link)
         // copy to clipboard
@@ -43,60 +43,62 @@ export class SharingComponent implements OnInit {
     }
 
     sendUnsharingConfirmation(): void {
-        this.confirmPopup.type = "noteUnshare"
-        this.confirmPopup.open = true
-        this.confirmPopup.onSuccess = this.unshare.bind(this)
+        this.confirmPopup.type = 'noteUnshare';
+        this.confirmPopup.open = true;
+        this.confirmPopup.onSuccess = this.unshare.bind(this);
     }
 
     unshare() {
         this.requests.notes.unshare.onSuccess =
-            this.removeSharingToken.bind(this)
-        this.loaderVisible = true
-        this.requests.notes.unshare.send(this.activeNote)
+            this.removeSharingToken.bind(this);
+        this.loaderVisible = true;
+        this.requests.notes.unshare.send(this.activeNote);
     }
 
     removeSharingToken(): void {
-        this.activeNote.sharingToken = ""
-        this.activeNote.isShared = false
-        this.loaderVisible = false
-        this.sharingCloseEvent.emit()
+        this.activeNote.sharingToken = '';
+        this.activeNote.isShared = false;
+        this.loaderVisible = false;
+        this.sharingCloseEvent.emit();
     }
 
     sendRevokingConfirmation(): void {
-        this.confirmPopup.type = "linkRevoke"
-        this.confirmPopup.open = true
-        this.confirmPopup.onSuccess = this.revoke.bind(this)
+        this.confirmPopup.type = 'linkRevoke';
+        this.confirmPopup.open = true;
+        this.confirmPopup.onSuccess = this.revoke.bind(this);
     }
 
     revoke(): void {
         this.requests.notes.revoke.onSuccess =
-            this.revokeSharingToken.bind(this)
-        this.loaderVisible = true
-        this.requests.notes.revoke.send(this.activeNote)
+            this.revokeSharingToken.bind(this);
+        this.loaderVisible = true;
+        this.requests.notes.revoke.send(this.activeNote);
     }
 
     revokeSharingToken(backendResponse: backend_notes_response): void {
-        const sharingToken = backendResponse.notes[0].sharing_token
-        this.activeNote.sharingToken = sharingToken
-        this.loaderVisible = false
-        this.notifyAboutRevokedLink()
+        const sharingToken = backendResponse.notes[0].sharing_token;
+        this.activeNote.sharingToken = sharingToken;
+        this.loaderVisible = false;
+        this.notifyAboutRevokedLink();
     }
 
     notifyAboutRevokedLink(): void {
-        this.materialNotification.open("Link revoked.", "Close", {
+        this.materialNotification.open('Link revoked.', 'Close', {
             duration: 5000,
-            panelClass: ["notification"],
-        })
+            panelClass: ['notification'],
+        });
     }
 
     notifyAboutCopiedText() {
+        console.log('copied');
+
         this.materialNotification.open(
-            "Link copied. Now send it to somebody :)",
-            "Close",
+            'Link copied. Now send it to somebody :)',
+            'Close',
             {
                 duration: 5000,
-                panelClass: ["notification"],
+                panelClass: ['notification'],
             }
-        )
+        );
     }
 }
