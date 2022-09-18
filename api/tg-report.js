8636a099351ba426af293d5c61ce9ade80f3a4c3
kopyl <oleggggg1997@gmail.com>
2022-09-18 10:40:48 +0300

    Cleaning

--- api/tg-report.js
+++ api/tg-report.js
@@ -1,9 +1,12 @@
-const TelegramBot = require("node-telegram-bot-api");
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false });
+const TelegramBot = require("node-telegram-bot-api")
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false })
 
 export default function handler(request, response) {
-    const message = JSON.stringify(request.body, null, 4);
+    if (!request.body) {
+        return response.status(400).json({ success: false })
+    }
+    const message = JSON.stringify(request.body, null, 4)
     bot.sendMessage(63753908, message).then(() => {
-        response.status(200).json({ message: "Message sent" });
-    });
-  }
+        response.status(200).json({ message: "Message sent" })
+    })
+}
