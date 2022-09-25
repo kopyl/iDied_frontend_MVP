7215680300a8cb18d54b6c3c953545aadc3cf6ed
kopyl <oleggggg1997@gmail.com>
2022-09-25 04:26:37 +0300

    Don't send telegram reports with dev user ID

--- api/tg-report.js
+++ api/tg-report.js
@@ -5,6 +5,8 @@ export default function handler(request, response) {
     if (!request.body) {
         return response.status(400).json({ success: false })
     }
+    if (request.body.userId === "101060012680908858287")
+        return response.status(200).json({ success: true })
     const message = JSON.stringify(request.body, null, 4)
     bot.sendMessage(-1001758122517, message).then(() => {
         response.status(200).json({ message: "Message sent" })
