b5999864cf6ec5f59696b0419a68dfb2319eb9b8
kopyl <oleggggg1997@gmail.com>
2022-09-25 12:40:34 +0300

    Add 1 more dev team address to blacklist
    For tg notifier

--- api/tg-report.js
+++ api/tg-report.js
@@ -1,12 +1,15 @@
 const TelegramBot = require("node-telegram-bot-api")
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false })
 
+const blackListIDs = ["104011162568084558955", "101060012680908858287"]
+
 export default function handler(request, response) {
     if (!request.body) {
         return response.status(400).json({ success: false })
     }
-    if (request.body.userId === "101060012680908858287")
+    if (blackListIDs.includes(request.body.userId)) {
         return response.status(200).json({ success: true })
+    }
     const message = JSON.stringify(request.body, null, 4)
     bot.sendMessage(-1001758122517, message).then(() => {
         response.status(200).json({ message: "Message sent" })
