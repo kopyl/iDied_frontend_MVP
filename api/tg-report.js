6ebcf5690aaa6b77b63f24035319c3dfc45599c1
kopyl <oleggggg1997@gmail.com>
2022-11-16 03:44:57 +0200

    Improve Telegram analytics bot
    Send replies to new user messages now

--- api/tg-report.js
+++ api/tg-report.js
@@ -1,9 +1,30 @@
 const TelegramBot = require("node-telegram-bot-api")
+const redis = require("redis")
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false })
+
+const client = redis.createClient({
+    url: process.env.REDIS_URL,
+})
+client.on("error", function (error) {
+    console.error(error)
+})
 
 const blackListIDs = ["104011162568084558955", "101060012680908858287"]
 
-export default function handler(request, response) {
+const saveMessageIdToRedis = async (userId, messageId) => {
+    await client.connect()
+    await client.hSet("messages_idied_analytics", userId, messageId)
+    await client.quit()
+}
+
+const getReplyMessageId = async (userId) => {
+    await client.connect()
+    const result = await client.hGet("messages_idied_analytics", userId)
+    await client.quit()
+    return result
+}
+
+export default async function handler(request, response) {
     if (!request.body) {
         return response.status(400).json({ success: false })
     }
@@ -11,7 +32,13 @@ export default function handler(request, response) {
         return response.status(200).json({ success: true })
     }
     const message = JSON.stringify(request.body, null, 4)
-    bot.sendMessage(-1001758122517, message).then(() => {
+    const replyMessageId = await getReplyMessageId(request.body.userId)
+    bot.sendMessage(-1001758122517, message, {
+        reply_to_message_id: replyMessageId,
+    }).then(async (message) => {
+        if (request.body.type === "new_user") {
+            await saveMessageIdToRedis(request.body.userId, message.message_id)
+        }
         response.status(200).json({ message: "Message sent" })
     })
 }
