f527d6e222a705504d7ee7f619f48852821fa951
kopyl <oleggggg1997@gmail.com>
2022-11-20 04:24:42 +0200

    Make conditionally highlighting buttons better
    https://t.me/designerschannel/8605

--- src/app/components/icons/icon-share/icon-share.component.ts
+++ src/app/components/icons/icon-share/icon-share.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'icon-share',
-  templateUrl: './icon-share.component.html',
-  styleUrls: ['./icon-share.component.sass']
+    selector: 'icon-share',
+    templateUrl: './icon-share.component.html',
+    styleUrls: ['./icon-share.component.sass'],
 })
 export class IconShareComponent implements OnInit {
+    @Input('color') color: string = '#878179'
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
