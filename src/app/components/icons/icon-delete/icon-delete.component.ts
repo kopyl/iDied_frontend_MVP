e4f5abb140fa6f3ec99f7b6e7040ac46624dc76f
kopyl <oleggggg1997@gmail.com>
2022-12-01 19:23:58 +0200

    Change button's icon color

--- src/app/components/icons/icon-delete/icon-delete.component.ts
+++ src/app/components/icons/icon-delete/icon-delete.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, Input } from "@angular/core"
+import { Component, OnInit, Input } from '@angular/core'
 
 @Component({
-    selector: "icon-delete",
-    templateUrl: "./icon-delete.component.html",
-    styleUrls: ["./icon-delete.component.sass"],
+    selector: 'icon-delete',
+    templateUrl: './icon-delete.component.html',
+    styleUrls: ['./icon-delete.component.sass'],
 })
 export class IconDeleteComponent implements OnInit {
-
-    @Input('color') color: string = '#FF5C59'
+    @Input('color') color: string = '#878179'
 
     constructor() {}
 
