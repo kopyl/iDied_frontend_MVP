298f5ed22a98f86e575badab155c7b47b57795e2
kopyl <oleggggg1997@gmail.com>
2022-12-02 16:24:56 +0200

    Add more types to lang service

--- src/app/services/lang/lang.service.ts
+++ src/app/services/lang/lang.service.ts
@@ -3,6 +3,15 @@ import copy, { LocaleKey } from 'src/locale'
 import { ConfirmPopupComponent } from '@components/confirmation-popup'
 
 const defaultLocale = 'ua'
+const locales = ['ua', 'en', 'ru']
+const flags = {
+    ua: 'ua-flag.svg',
+    en: 'en-flag.svg',
+    ru: 'ru-flag.svg',
+} as const
+
+type LocaleCode = 'ua' | 'en' | 'ru'
+type Flag = typeof flags[keyof typeof flags]
 
 @Injectable({
     providedIn: 'root',
@@ -14,29 +23,27 @@ export class LangService {
         return copy[this.lang]
     }
 
-    private _lang: string = defaultLocale
+    private _lang: LocaleCode = defaultLocale
 
-    public get lang(): string {
+    public get lang(): LocaleCode {
         if (this._lang) return this._lang
-        const fromStorage = localStorage.getItem('lang')
+        const fromStorage = localStorage.getItem('lang') as LocaleCode
         this.lang = fromStorage || defaultLocale
         return fromStorage || defaultLocale
     }
 
-    public get flag(): string {
-        if (this.lang === 'ua') return 'ua-flag.svg'
-        if (this.lang === 'ru') return 'ru-flag.svg'
-        return 'en-flag.svg'
+    public get flag(): Flag {
+        return flags[this.lang]
     }
 
-    public set lang(lang: string) {
+    public set lang(lang: LocaleCode) {
         if (this._lang === lang) return
-        if (lang !== 'en' && lang !== 'ua' && lang !== 'ru') return
+        if (!locales.includes(lang)) return
         this._lang = lang
         localStorage.setItem('lang', lang)
     }
 
-    public setLang = (lang: string): void => {
+    public setLang = (lang: LocaleCode): void => {
         this.lang = lang
         this.confirmPopup.open = false
     }
